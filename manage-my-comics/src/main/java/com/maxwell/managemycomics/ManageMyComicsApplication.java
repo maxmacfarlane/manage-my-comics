9e458f5843dc4aaeea6098b3c9ef5a070555cbc9
Maxwell MacFarlane <maxwell.macfarlane@gmail.com>
2023-05-31 13:06:42 -0600

    project base

---
+++ manage-my-comics/src/main/java/com/maxwell/managemycomics/ManageMyComicsApplication.java
@@ -0,0 +1,13 @@
+package com.maxwell.managemycomics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageMyComicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageMyComicsApplication.class, args);
+	}
+
+}
---
+++ manage-my-comics/src/test/java/com/maxwell/managemycomics/ManageMyComicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxwell.managemycomics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageMyComicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
